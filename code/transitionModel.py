4914252a73b97dbfcbc795338838bf8e4c0e67f1
fatima-zahra-bourouba <fatima.zahra.bourouba@ensia.edu.dz>
2024-04-05 13:46:54 +0200

    begin

---
+++ code/transitionModel.py
@@ -0,0 +1,92 @@
+
+transition_model = {
+    'change conditions ':{
+        'Wheat': {
+            'Poor': 5, 
+            'Below average': 10,
+            'Average': 20,
+            'Above Average': 35,
+            'Ideal': 50
+        },
+        'Corn': {
+            'Poor': 15,
+            'Below average': 20,
+            'Average': 40,
+            'Above Average': 60,
+            'Ideal': 80
+        },
+        'Dates': {
+            'Poor': 15,
+            'Below average':20,
+            'Average': 40,
+            'Above Average': 60,
+            'Ideal': 80
+        },
+        'Potatoes': {
+            'Poor': 50,
+            'Below average':100, 
+            'Average':200,
+            'Above Average': 300,
+            'Ideal': 400
+        },
+        'Tomatoes': {
+            'Poor': 150,
+            'Below average':200,
+            'Average': 400,
+            'Above Average':600, 
+            'Ideal': 800
+        },
+        'Green Pepper': {
+            'Poor': 5,
+            'Below average': 100,
+            'Average': 200,
+            'Above Average': 300,
+            'Ideal': 400
+        },
+        'Aubergines': {
+            'Poor':5,
+            'Below average':100, 
+            'Average': 200,
+            'Above Average': 300,
+            'Ideal': 400
+        },
+        'Onions': {
+            'Poor': 5,
+            'Below average':100,
+            'Average':200, 
+            'Above Average':300, 
+            'Ideal': 400
+        },
+        'Carrots': {
+            'Poor': 5,
+            'Below average':200,
+            'Average':400, 
+            'Above Average':600, 
+            'Ideal': 800
+        },
+        'Lemon': {
+            'Poor': 2,
+            'Below average':100, 
+            'Average':200,
+            'Above Average':300, 
+            'Ideal': 400
+        },
+        'Olives': {
+            'Poor': 2,
+            'Below average':10, 
+            'Average':20, 
+            'Above Average':40, 
+            'Ideal': 60
+        },
+        'Apples': {
+            'Poor': 2,
+            'Below average':100, 
+            'Average':200, 
+            'Above Average':300, 
+            'Ideal': 400
+        }
+
+    },
+    'change_land' : {-0.15,-0.1 ,-0.05,0,0.05,0.10,0.15} #the pecentages of changes
+
+}
