c864e40ae363c7daef5e26bbeb5f6161807dcc90
randa-ensia <basmala.randa.benmaiche@ensia.edu.dz>
2024-04-18 18:23:09 +0100

    Update node.py

--- code/problemFormalization/node.py
+++ code/problemFormalization/node.py
@@ -1,22 +1,20 @@
 class Node:
-    def __init__(self, state, parent=None, action=None, cost=0, heuristic_cost=0):
+    def __init__(self, state, parent=None, action=None, cost=0, total_cost = 0):
         self.state = state
         self.parent = parent
         self.action = action
         self.cost = cost
-        self.heuristic_cost = heuristic_cost
+        self.total_cost = total_cost
         if parent is None:
             self.depth = 0
         else:
             self.depth = parent.depth + 1
 
-    def total_cost(self):
-        return self.cost + self.heuristic_cost
 
     def __hash__(self):
-        hashable = tuple(self.state)
+        return self.state
         #print(type(hashable))
-        return hash(hashable) #because dict are not hashable make it a tuple to hash it
+    
     def __str__(self):
         return str(self.state)
 
@@ -24,4 +22,4 @@ class Node:
         return self.state == other.state
 
     def __lt__(self, other):
-        return self.total_cost() < other.total_cost()
+        return self.cost < other.cost
